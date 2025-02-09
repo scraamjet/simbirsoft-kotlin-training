535288cc1f33c0a6e9e4b5d21e96901749a4fa86
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-09 16:24:15 +0300

    Handle auth callback based on user age check

--- app/src/main/java/com/example/simbirsoft_kotlin_training/AuthUtils.kt
+++ app/src/main/java/com/example/simbirsoft_kotlin_training/AuthUtils.kt
@@ -5,3 +5,22 @@ inline fun auth(updateCache: () -> Unit) {
     updateCache()
     println("Authentication process completed.")
 }
+
+inline fun auth(
+    user: User,
+    authCallback: AuthCallback,
+    updateCache: () -> Unit
+) {
+    println("Authenticating user ${user.name}...")
+    user.checkAdult()
+        .fold(
+            onSuccess = {
+                authCallback.authSuccess()
+                updateCache()
+            },
+            onFailure = {
+                authCallback.authFailed()
+            }
+        )
+    println("Authentication process completed.")
+}
